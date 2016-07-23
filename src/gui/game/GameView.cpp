252fb91b6aa655c861f80dfd1cc91179129d0ca2
SopaXorzTaker <markboldyrev@gmail.com>
2016-07-23 21:48:48 +0300

    Stylize text

--- src/gui/game/GameView.cpp
+++ src/gui/game/GameView.cpp
@@ -614,7 +614,7 @@ void GameView::NotifyMenuListChanged(GameModel * sender)
 			tempString += menuList[i]->GetIcon();
 			std::string description = menuList[i]->GetDescription();
 			if (i == SC_FAVORITES && Favorite::Ref().GetFavoritesList()->size() == 0)
-				description += " (Use ctrl+shift+click to favorite an element)";
+				description += " (Use Ctrl+Shift + left-click to favorite an element)";
 			ui::Button * tempButton = new ui::Button(ui::Point(WINDOWW-16, currentY), ui::Point(15, 15), tempString, description);
 			tempButton->Appearance.Margin = ui::Border(0, 2, 3, 2);
 			tempButton->SetTogglable(true);
